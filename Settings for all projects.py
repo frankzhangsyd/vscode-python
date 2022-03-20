5da7496d2169961c03c85d9cd4fdbdc159a8ef7b
frankzhangsyd <frank.dingrui@gmail.com>
2022-03-20 21:38:15 +1100

    Upload common settings

---
+++ Settings for all projects.py
@@ -0,0 +1,20 @@
+'''
+Author: your name
+Date: 2022-03-20 21:36:02
+LastEditTime: 2022-03-20 21:37:41
+LastEditors: Please set LastEditors
+Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+FilePath: \vscode python\Settings for all projects.py
+'''
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+import statsmodels as sm
+
+
+pd.set_option('display.max_columns', None)
+pd.options.display.float_format = '{:.2f}'.format
+pd.set_option("display.max_rows", 150)
+
